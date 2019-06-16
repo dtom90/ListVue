599cc7cbaa1bb4091fa0af13ae43c6477c456331
David Thomason <dtom90@users.noreply.github.com>
2019-06-16 18:05:11 -0400

    Testing both incomplete and complete Tasks

--- tests/unit/Task.spec.js
+++ tests/unit/Task.spec.js
@@ -7,13 +7,13 @@ localVue.component('font-awesome-icon', FontAwesomeIcon)
 
 import Task from '@/components/Task.vue'
 
-describe('Task.vue', () => {
+describe('Task', () => {
   
-  it('renders task.name and task.createdDate when passed', () => {
+  describe('Incomplete', () => {
     
     const task = {
-      id: 1, 
-      name: 'new task 1', 
+      id: 1,
+      name: 'new task 1',
       createdDate: new Date(),
       completedDate: null,
       completed: false
@@ -23,11 +23,64 @@ describe('Task.vue', () => {
       propsData: { task: task },
       localVue
     })
+
+    it('renders the task name', () => {
+
+      expect(wrapper.text()).toMatch(task.name)
+
+    })
+
+    it('renders the created date', () => {
+
+      expect(wrapper.text()).toMatch('Created on')
+      expect(wrapper.text()).toMatch(moment(task.createdDate).format('ddd MMM DD YYYY,'))
+      expect(wrapper.text()).toMatch(moment(task.createdDate).format('h:mm a'))
+
+    })
     
-    expect(wrapper.text()).toMatch(task.name)
-    expect(wrapper.text()).toMatch('Created on')
-    expect(wrapper.text()).toMatch(moment(task.createdDate).format('ddd MMM DD YYYY,'))
-    expect(wrapper.text()).toMatch(moment(task.createdDate).format('h:mm a'))
+    it('does not render "Created On"', () => {
+
+      expect(wrapper.text()).not.toMatch('Completed on')
+      
+    })
+    
+  })
+  
+  describe('Complete', () => {
+
+    const task = {
+      id: 1,
+      name: 'new task 1',
+      createdDate: new Date(),
+      completedDate: moment(new Date()).add(30, 'm').toDate(),
+      completed: true
+    }
+    
+    const wrapper = shallowMount(Task, {
+      propsData: { task: task },
+      localVue
+    })
+    
+    it('renders the task name', () => {
+
+      expect(wrapper.text()).toMatch(task.name)
+
+    })
+    
+    it('renders the completed date', () => {
+      
+      expect(wrapper.text()).toMatch('Completed on')
+      expect(wrapper.text()).toMatch(moment(task.completedDate).format('ddd MMM DD YYYY,'))
+      expect(wrapper.text()).toMatch(moment(task.completedDate).format('h:mm a'))
+
+    })
+
+    it('does not render the created date', () => {
+
+      expect(wrapper.text()).not.toMatch('Created on')
+      expect(wrapper.text()).not.toMatch(moment(task.createdDate).format('h:mm a'))
+
+    })
     
   })
   
