f0fc11dead336778f4305d1b4eaf0417ecf3eb7b
David Thomason <dtom90@users.noreply.github.com>
2020-01-17 18:51:40 -0500

    Updated so tests pass

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -12,22 +12,25 @@ const mutations = {
   },
 
   completeTask (state, id) {
-    const task = state.tasks.find(t => t.id === id)
+    const list = state.lists.find(list => list.name === state.selected)
+    const task = list.tasks.find(t => t.id === id)
     if (task.completed) { task.completedDate = Date.now() } else { task.completedDate = null }
   },
 
   deleteTask (state, id) {
-    const index = state.tasks.findIndex(t => t.id === id)
-    const task = state.tasks[index]
+    const list = state.lists.find(list => list.name === state.selected)
+    const index = list.tasks.findIndex(t => t.id === id)
+    const task = list.tasks[index]
     if (task.completed || confirm(`Are you sure you want to delete task ${task.name}? the task is not yet complete!`)) {
-      state.tasks.splice(index, 1)
+      list.tasks.splice(index, 1)
     }
   },
 
   clearTasks (state) {
-    const completedTasks = state.tasks.filter(t => t.completed)
+    const list = state.lists.find(list => list.name === state.selected)
+    const completedTasks = list.tasks.filter(t => t.completed)
     if (completedTasks.length === 1 || confirm(`Are you sure that you want to delete all ${completedTasks.length} completed tasks?`)) {
-      state.tasks = state.tasks.filter(t => !t.completed)
+      list.tasks = list.tasks.filter(t => !t.completed)
     }
   }
 }
