024ab53eeaf34ba987b15a213bcbaf2c93493051
David Thomason <dtom90@users.noreply.github.com>
2020-01-18 18:03:52 -0500

    Implemented ListNav.vue in nav drawer

---
+++ src/components/ListNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-list nav>
+    <v-list-item-group
+      v-model="selectedList"
+      link
+    >
+      <v-list-item
+        v-for="list in lists"
+        :key="list"
+      >
+        <v-list-item-action>
+          <v-icon />
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ list.name }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'ListNav',
+  computed: {
+    ...mapState([
+      'lists',
+      'selectedList'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
