fff8fba06228db56f0c2308a93ae0b704481eb76
David Thomason <dtom90@users.noreply.github.com>
2020-04-12 15:26:57 -0400

    Added Task scaffold

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   scope '/api' do
     resources :lists
+    resources :tasks
   end
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_221604) do
+ActiveRecord::Schema.define(version: 2020_04_12_192310) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -18,4 +18,14 @@ ActiveRecord::Schema.define(version: 2020_04_07_221604) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
+  add_foreign_key "tasks", "lists"
 end
