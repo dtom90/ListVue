ab0b754405087290e140f77f5a45901163b9643d
David Thomason <dtom90@users.noreply.github.com>
2020-04-29 07:04:41 -0400

    rails generate devise User

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   scope '/api' do
     resources :lists
     delete '/lists/:id/tasks/completed', to: 'lists#destroy_completed_tasks'
