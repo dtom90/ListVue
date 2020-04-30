8f257fdba14fb857bad4e133c533e57284df8efe
David Thomason <dtom90@users.noreply.github.com>
2020-04-30 18:52:19 -0400

    Added new controllers, routes, jbuilder partials

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope ':api' do
-    devise_for :users
+  scope 'api' do
+    
+    devise_for :users,
+               controllers: { sessions: :sessions },
+               path_names:  { sign_in: :login }
+
+    resource :user, only: [:show, :update]
+    
     resources :lists
     delete '/lists/:id/tasks/completed', to: 'lists#destroy_completed_tasks'
     delete '/lists/:id/tasks', to: 'lists#destroy_tasks'
