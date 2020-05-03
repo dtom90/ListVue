9a26a89c9778497fa207c8b7510384dbfc1a0c7d
David Thomason <dtom90@users.noreply.github.com>
2020-05-03 08:43:55 -0400

    Reorganized App.vue into separate components Removed .idea files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,105 +1,37 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mobile-break-point="$vuetify.breakpoint.thresholds.xs"
-      clipped
-      app
-    >
-      <ListNav />
-    </v-navigation-drawer>
+    <AppBar @toggleDrawer="drawer = !drawer" />
     
-    <v-app-bar
-      clipped-left
-      app
-      color="blue darken-3"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title
-        style="width: 300px"
-        class="ml-0 pl-4"
-      >
-        <span>ListVue</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <div v-if="!email">
-        <LoginDialog />
-      </div>
-      <div v-if="email">
-        <span>Logged in as {{ email }} </span>
-        <v-btn
-          color="blue"
-          @click="logOut"
-        >
-          Log Out
-        </v-btn>
-      </div>
-    </v-app-bar>
+    <NavigationDrawer
+      :nav-drawer="drawer"
+      @setDrawer="drawer = $event"
+    />
     
-    <v-content>
-      <v-container fluid>
-        <v-row justify="center">
-          <v-col>
-            <div class="list-container">
-              <div v-if="selectedList">
-                <List
-                  :title="selectedList.name"
-                  :tasks="incompleteTasks"
-                  :created-date="selectedList.created_at"
-                />
-                <br>
-                <br>
-                <List
-                  v-if="completedTasks.length > 0"
-                  title="Completed"
-                  :tasks="completedTasks"
-                />
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+    <AppContent />
   </v-app>
 </template>
 
 <script>
-import LoginDialog from './components/LoginDialog'
-import ListNav from './components/ListNav'
-import List from './components/List'
-import { mapState, mapGetters, mapActions } from 'vuex'
+import AppBar from './components/AppBar'
+import NavigationDrawer from './components/NavigationDrawer'
+import AppContent from './components/AppContent'
+import { mapActions } from 'vuex'
 
 export default {
-  components: { LoginDialog, ListNav, List },
+  components: { AppBar, NavigationDrawer, AppContent },
   data: () => ({
     drawer: null
   }),
-  computed: {
-    ...mapState([
-      'email'
-    ]),
-    ...mapGetters([
-      'selectedList',
-      'incompleteTasks',
-      'completedTasks'
-    ])
-  },
   created () {
     this.checkSignIn()
   },
   methods: {
     ...mapActions([
-      'checkSignIn',
-      'logOut'
-    ])
+      'checkSignIn'
+    ]),
+    toggleDrawer () {
+      this.drawer = !this.drawer
+    }
   }
 }
 </script>
-
-<style>
-.list-container {
-  max-width: 700px;
-  margin: auto;
-}
-</style>
---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app-bar
+    clipped-left
+    app
+    color="blue darken-3"
+    dark
+  >
+    <v-app-bar-nav-icon @click.stop="$emit('toggleDrawer')" />
+    <v-toolbar-title
+      style="width: 300px"
+      class="ml-0 pl-4"
+    >
+      <span>ListVue</span>
+    </v-toolbar-title>
+    <v-spacer />
+    <div v-if="!email">
+      <LoginDialog />
+    </div>
+    <div v-if="email">
+      <span>Logged in as {{ email }} </span>
+      <v-btn
+        color="blue"
+        @click="logOut"
+      >
+        Log Out
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+import LoginDialog from './LoginDialog'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'AppBar',
+  components: { LoginDialog },
+  props: {
+    toggleDrawer: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'email'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'logOut'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
