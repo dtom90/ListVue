779fe2ea4bf6ef8c861d511ef4d0b421c69c813a
David Thomason <dtom90@users.noreply.github.com>
2020-05-20 21:07:07 -0400

    Gracefully handle API request errors in frontend

--- frontend/src/store/request.js
+++ frontend/src/store/request.js
@@ -1,16 +1,24 @@
 const BASE_PATH = '/api'
 
-const send = (method, path, requestData = null) => new Promise(resolve =>
+const send = (method, path, requestData = null) => new Promise((resolve, reject) =>
   fetch(BASE_PATH + path, {
     method,
     headers: Object.assign({ 'Content-Type': 'application/json' },
       'token' in window.localStorage ? { 'Authorization': 'Token ' + window.localStorage.token } : {}),
     body: requestData === null ? null : JSON.stringify(requestData)
   })
+    .then(response => {
+      if (response.ok) {
+        return response
+      }
+      throw response
+    })
     .then(stream => stream.json())
-    .then(responseData => resolve(responseData))
+    .then(json => resolve(json))
     .catch(error => {
-      throw new Error(`API ${error}`)
+      // eslint-disable-next-line no-console
+      console.error(error)
+      reject(error)
     })
 )
 
@@ -20,21 +28,6 @@ const post = (path, data) => send('POST', path, data)
 
 const patch = (path, data) => send('PATCH', path, data)
 
-const _delete = (path) => new Promise((resolve, reject) =>
-  fetch(BASE_PATH + path, {
-    method: 'DELETE',
-    headers: 'token' in window.localStorage ? { 'Authorization': 'Token ' + window.localStorage.token } : {}
-  })
-    .then(response => {
-      if (response.ok) {
-        resolve()
-      } else {
-        reject(response.statusText)
-      }
-    })
-    .catch(error => {
-      throw new Error(`API ${error}`)
-    })
-)
+const _delete = (path) => send('DELTE', path)
 
 export { get, post, patch, _delete }
